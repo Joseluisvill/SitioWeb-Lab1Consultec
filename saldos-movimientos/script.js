11ccbbc45f3ffad05075512402c08b2c7087498b
Joseluisvill <48971206+Joseluisvill@users.noreply.github.com>
2024-02-22 10:00:33 -0500

    Update Saldos Movimientos script.js

--- saldos-movimientos/script.js
+++ saldos-movimientos/script.js
@@ -1,15 +1,15 @@
 function saldosMovimientos() {
   window.location.href =
-    "https://joseluisvill.github.io/LabWebAcademaConsultec-IT/saldos-movimientos/SaldosMovimientos.html";
+    "https://joseluisvill.github.io/SitioWeb-Lab1Consultec/saldos-movimientos/SaldosMovimientos.html";
 }
 const Div = document.getElementById("div1");
 const Div2 = document.getElementById("div2");
 
 Div.addEventListener("click", function () {
   window.location.href =
-    "https://joseluisvill.github.io/LabWebAcademaConsultec-IT/movimientos/movimientos.html";
+    "https://joseluisvill.github.io/SitioWeb-Lab1Consultec/movimientos/movimientos.html";
 });
 Div2.addEventListener("click", function () {
   window.location.href =
-    "https://joseluisvill.github.io/LabWebAcademaConsultec-IT/movimientos/movimientos.html";
+    "https://joseluisvill.github.io/SitioWeb-Lab1Consultec/movimientos/movimientos.html";
 });
